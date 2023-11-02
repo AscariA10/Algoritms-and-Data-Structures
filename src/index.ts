dcf3bf3d810933181774b8ab03d11366cb09f967
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-11-02 18:35:38 +0200

    add lesson

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,42 @@ if (button && input) {
 type Person = { name: string; age?: number };
 let user: Person = { name: "Alice" };
 user.age = 27;
+
+type Dog = {
+   bark: () => void;
+};
+
+type Cat = {
+   meow: () => void;
+};
+
+function isDog(animal: Dog | Cat): animal is Dog {
+   return "bark" in animal;
+}
+
+function isCat(animal: Dog | Cat): animal is Cat {
+   return "meow" in animal;
+}
+
+function letAnimalTalk(animal: Dog | Cat) {
+   if (isDog(animal)) {
+      animal.bark();
+   } else if (isCat(animal)) {
+      animal.meow();
+   }
+}
+
+const thomas: Cat = {
+   meow: () => {
+      console.log("meowmeow");
+   },
+};
+
+const lobo: Dog = {
+   bark: () => {
+      console.log("gavgav");
+   },
+};
+
+letAnimalTalk(thomas);
+letAnimalTalk(lobo);
