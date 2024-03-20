13045409011e53a3d9072eb4ea824938a3617c70
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2024-03-20 19:58:47 +0200

    complete task 5

--- src/algorithms/arrays-and-strings/task5.ts
+++ src/algorithms/arrays-and-strings/task5.ts
@@ -9,10 +9,32 @@ const testString5: string = "bake";
 type TcheckModFunc = (string1: string, string2: string) => boolean;
 
 const oneEditReplace: TcheckModFunc = (str1, str2) => {
+   let isDifferent: boolean = false;
+   for (let i = 0; i < str1.length; i += 1) {
+      if (str1[i] !== str2[i]) {
+         if (isDifferent) {
+            return false;
+         }
+         isDifferent = true;
+      }
+   }
    return true;
 };
 
 const oneEditInsert: TcheckModFunc = (str1, str2) => {
+   let index1 = 0;
+   let index2 = 0;
+   while (index2 < str2.length && index1 < str1.length) {
+      if (str1[index1] !== str2[index2]) {
+         if (index1 !== index2) {
+            return false;
+         }
+         index2 += 1;
+      } else {
+         index1 += 1;
+         index2 += 1;
+      }
+   }
    return true;
 };
 
@@ -20,9 +42,12 @@ const checkSimilarity: TcheckModFunc = (firstStr, secondStr) => {
    if (firstStr.length === secondStr.length) {
       return oneEditReplace(firstStr, secondStr);
    } else if (firstStr.length + 1 === secondStr.length) {
-      oneEditInsert(firstStr, secondStr);
+      return oneEditInsert(firstStr, secondStr);
    } else if (firstStr.length - 1 === secondStr.length) {
-      oneEditInsert(secondStr, firstStr);
+      return oneEditInsert(secondStr, firstStr);
    }
    return false;
 };
+
+console.log(checkSimilarity(testString1, testString4));
+console.log(checkSimilarity(testString1, testString2));
