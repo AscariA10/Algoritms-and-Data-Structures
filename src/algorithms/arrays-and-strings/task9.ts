11bc6357db8eb5b730e5485d589f7354f8c5b802
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2024-03-26 23:56:54 +0200

    1-9 task done

---
+++ src/algorithms/arrays-and-strings/task9.ts
@@ -0,0 +1,14 @@
+// ? is string equal to another by cyclic shift
+
+type TcyclicShift = (string1: string, string2: string) => boolean;
+
+const cyclicShiftChecker: TcyclicShift = (str1, str2) => {
+   if (str1.length != str2.length) {
+      return false;
+   }
+
+   const concatenatedString = str1 + str1;
+   return concatenatedString.includes(str2);
+};
+
+console.log(cyclicShiftChecker("string", "rinstg"));
