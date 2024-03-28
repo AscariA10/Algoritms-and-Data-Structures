1fe126b73e60640eed69188ba401e395cb677386
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2024-03-28 18:48:15 +0200

    add and test dLL logic

--- src/algorithms/linked-list/task1.ts
+++ src/algorithms/linked-list/task1.ts
@@ -1,8 +1,27 @@
 import { testLinkedList, emptyLinkedList, LinkedList } from "../../data-structures/linked-list";
 
-const linkedList = new LinkedList(null);
-linkedList.add(5);
-linkedList.add(10);
-linkedList.add(15);
+import { DoubleLinkedList } from "../../data-structures/double-linked-list";
 
-console.log(linkedList);
+// const linkedList = new LinkedList(null);
+// linkedList.add(5);
+// linkedList.add(10);
+// linkedList.add(15);
+
+// console.log(linkedList);
+
+// interface Post {
+//    title: string;
+// }
+
+const myDoubleLinkedList = new DoubleLinkedList<string>();
+myDoubleLinkedList.append("0");
+myDoubleLinkedList.append("1");
+myDoubleLinkedList.append("2");
+myDoubleLinkedList.append("3");
+const traverse = myDoubleLinkedList.traverse();
+const node = myDoubleLinkedList.searchNode(title => title === "1");
+
+console.log(myDoubleLinkedList);
+
+myDoubleLinkedList.deleteNode(node);
+console.log(myDoubleLinkedList.traverse());
--- src/data-structures/double-linked-list.ts
+++ src/data-structures/double-linked-list.ts
@@ -2,10 +2,10 @@ interface IdoubleLinkedList<T> {
    prepend(data: T): NodeD<T>;
    append(data: T): NodeD<T>;
    getLastNode(node: NodeD<T>): NodeD<T>;
-   //    deleteNode(node: NodeD<T>): void;
-   //    traverse(): T[];
-   //    size(): number;
-   //    searchNode(comparator: (data: T) => boolean): NodeD<T> | null;
+   deleteNode(node: NodeD<T> | null): void;
+   traverse(): NodeD<T>[];
+   size(): number;
+   searchNode(comparator: (data: T) => boolean): NodeD<T> | null;
 }
 
 interface IdoubleNode<T> {
@@ -25,7 +25,7 @@ class NodeD<T> {
    constructor(public nodeData: T) {}
 }
 
-class DoubleLinkedList<T> implements IdoubleLinkedList<T> {
+export class DoubleLinkedList<T> implements IdoubleLinkedList<T> {
    private head: NodeD<T> | null = null;
    getLastNode(node: NodeD<T>) {
       return node.next ? this.getLastNode(node.next) : node;
@@ -52,5 +52,39 @@ class DoubleLinkedList<T> implements IdoubleLinkedList<T> {
          newNode.prev = lastNode;
       }
       return newNode;
+   }
+   public deleteNode(node: any) {
+      const prevNode = node.prev;
+      const nextNode = node.next ? node.next : null;
+      if (!node.prev) {
+         this.head = node.next;
+      } else {
+         prevNode.next = node.next;
+         nextNode.prev = prevNode;
+      }
+   }
+   public traverse() {
+      const nodeArray: NodeD<T>[] = [];
+      if (!this.head) {
+         return nodeArray;
+      }
+      let startNode: NodeD<T> | null = this.head;
+      while (startNode) {
+         nodeArray.push(startNode);
+         startNode = startNode.next;
+      }
+      return nodeArray;
+   }
+   public size(): number {
+      return this.traverse().length;
+   }
+   public searchNode(comparator: (data: T) => boolean): NodeD<T> | null {
+      const checkNext = (node: NodeD<T>): NodeD<T> | null => {
+         if (comparator(node.nodeData)) {
+            return node;
+         }
+         return node.next ? checkNext(node.next) : null;
+      };
+      return this.head ? checkNext(this.head) : null;
    }
 }
